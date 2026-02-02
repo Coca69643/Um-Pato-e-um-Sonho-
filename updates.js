a649659eb72f8f15013f5b230487bbeb718ff501
YT | @Coca69643 <yutiraia@gmail.com>
2026-02-02 18:01:03 -0300

    Add files via upload

--- updates.js
+++ updates.js
@@ -40,10 +40,9 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
-
